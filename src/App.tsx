303e73cd777052768705cfbcb35f75912e191a14
ljyo2o9 <yggosxoljy@gmail.com>
2024-09-23 22:08:11 +0900

    update :: ui 고치기

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   };
 
   const onChangeDate = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if(e.target.value <= "0"){
+    if (e.target.value <= "0") {
       setDate(1);
     } else {
       setDate(Number(e.target.value));
@@ -31,9 +31,9 @@ function App() {
     return decodedText;
   };
 
-  const generateExcel = async (area:string, places:string, date:number): Promise<void> => {
+  const generateExcel = async (area: string, places: string, date: number): Promise<void> => {
     console.log("come");
-    
+
     await axios.post('http://localhost:3001/oyo/excel', {
       area: area,
       places: places,
@@ -41,7 +41,7 @@ function App() {
     }, { responseType: 'blob' }
     ).then((res) => {
       console.log(res.data);
-      
+
       const blob = new Blob([res.data], {
         type: res.headers['content-type'],
       });
@@ -55,47 +55,75 @@ function App() {
 
   return (
     <Main>
-      <Div>
-        <Text>여행 떠날 지역 : </Text>
-        <InputField type="text" name="area" value={area} onChange={onChangeArea}></InputField>
-      </Div>
+      <H1>오요</H1>
+      <H3>여행 계획을 짜기 귀찮을 때! 맡겨만 주세요</H3>
+      <Contents>
+        <Div>
+          <Text>여행 떠날 지역</Text>
+          <InputField type="text" name="area" value={area} onChange={onChangeArea}></InputField>
+        </Div>
 
-      <Div>
-        <Text>그 지역에서 꼭 가고 싶은 곳 : ,(쉼표)로 표시해주세요</Text>
-        <InputField type="text" name="places" value={places} onChange={onChangePlace}></InputField>
-      </Div>
+        <Div>
+          <Text>꼭 가고싶은 곳</Text>
+          <InputField type="text" name="places" value={places} onChange={onChangePlace}></InputField>
+        </Div>
 
-      <Div>
-        <Text>여행 일 수 :</Text>
-        <InputField type="number" name="date" value={date} onChange={onChangeDate}></InputField>
-      </Div>
+        <Div>
+          <Text>여행 일 수</Text>
+          <InputField type="number" name="date" value={date} onChange={onChangeDate}></InputField>
+        </Div>
+      </Contents>
 
-      <SubmitButton onClick={() => {generateExcel(area, places, date)}}>생성하기</SubmitButton>
+
+      <SubmitButton onClick={() => { generateExcel(area, places, date) }}>생성하기</SubmitButton>
     </Main>
   );
 }
 
 const Main = styled.div`
   display: flex;
+  margin : 0 auto;
+  width : 100%;
+  text-align : center;
   flex-direction: column;
   align-content: center;
-  gap: 20px;
-  width: 400px;
+  justify-content: center;
+  width: max-content;
 `;
 
-const Text = styled.p``
+const Contents = styled.div`
+  display: flex;
+  gap: 10px
+`
+
+const H1 = styled.h1``
+const H3 = styled.h3``
+
+const Text = styled.p`
+  margin: 0;
+  width: fit-content;
+`
 
 const Div = styled.div`
-  display: flex
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding-bottom: 20px;
+  gap: 10px;
 `;
 
 const InputField = styled.input`
   display: flex;
+  width: min-content;
+  text-align: center;
 `;
 
 const SubmitButton = styled.button`
   display: flex;
-  padding: 20px;
+  width: fit-content;
+  padding: 10px 20px;
   justify-self: center;
   align-self: center;
 `
